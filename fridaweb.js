be31c8953961fba697c6510b46bbe8f9f09788a1
Conny Dahlgren <shimmy@shoogle.se>
2016-08-20 18:11:51 +0200

    Tabs'n stuff

--- fridaweb.js
+++ fridaweb.js
@@ -1,11 +1,12 @@
 
 $( document ).ready(function() {
-   console.log( jsondata );
+	console.log( jsondata );
  	var source = $("#request-template").html();
  	var template = Handlebars.compile(source);
-
+ 	// Todo: sections to avoid repeating code in template
+ 	// var sections = ["Request", "Response"]
 	$.each(jsondata.trace, function(index,value) {
-
+		// Todo: detect body type, binary etc.
 		try {
 			var req_body_json = JSON.stringify(JSON.parse(value.request.body), null, 2); 
 		} catch(err) {
@@ -18,24 +19,30 @@ $( document ).ready(function() {
 		} catch(err) {
 			var resp_body_json = value.response.body;
 		}
+		// Template variables
+		var context = {id: value.ts, //Timestamp used to make unique ID's in HTML (Bad?)
+					  raw_response: JSON.stringify(value.response, null, 2), 
+					  raw_request: JSON.stringify(value.request, null, 2), 
+					  entry: value, 
+					  resp_body_json: resp_body_json, 
+					  req_body_json: req_body_json}
 
-		var context = {entry: value, resp_body_json: resp_body_json, req_body_json: req_body_json}
 		var html = template(context);
 		$("#container").append( html );
 	});
    
-   $(".panel-heading").click(function(e) {
-   	$(this).siblings().slideToggle();
-   	$(this).find(".expanded").toggle();
-   	$(this).find(".expand").toggle();
-   })
 
-   $(this).find(".expand").toggle();
+	// Hide the right arrow on load
+	$(this).find(".expand").toggle();
 
-   $(".title").click(function(e) {
-   	$(this).next().slideToggle();
-   	$(this).find(".expanded").toggle();
-   	$(this).find(".expand").toggle();
-   })
+	$(".panel-heading").click(function(e) {
+		$(this).siblings().slideToggle();
+		$(this).find(".expanded, .expand").toggle();
+	})
+
+	$(".title").click(function(e) {
+		$(this).next().slideToggle();
+		$(this).find(".expanded, .expand").toggle();
+	})
 
 });
